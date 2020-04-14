8e807003a654196b53ca23d5336767313400c281
Jamie Symonds-Tayler <jamie.s-t@outlook.com>
2020-04-14 11:15:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,12 @@
 def new_hash
   # return an empty hash
-  {}
+  hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  {
-    name: "minnie"
+  hash = {
+    name: "minnie",
     species: "cat"
   }
 end
